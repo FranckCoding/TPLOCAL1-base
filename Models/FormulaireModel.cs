252f10e2c9171326f6894541bb0baf59ab0ca5d1
FranckCoding <leger_franck@protonmail.com>
2024-12-11 16:16:51 +0100

    Fix some requirement of datas in model attributes.

--- Models/FormulaireModel.cs
+++ Models/FormulaireModel.cs
@@ -18,11 +18,11 @@ namespace TPLOCAL1.Models
         public string UserZipCode { get; set; }
         [Required(ErrorMessage ="La ville est obligatoire.")]
         public string UserTown { get; set; }
-        [RegularExpression("^([\\w] +)@([\\w] +)\\.([\\w] +)$", ErrorMessage ="Merci de respecter le format d'email exemple@exemple.com")]
+        [RegularExpression("^([\\w]+)@([\\w]+).([\\w]+)$", ErrorMessage ="Merci de respecter le format d'email exemple@exemple.com")]
         [Required(ErrorMessage ="L'adresse email est obligatoire.")]
         public string UserEmail { get; set; }
         [Required(ErrorMessage ="La date de début de formation est obligatoire.")]
-        [DataType(DataType.Date)]
+        [DataType(DataType.Date, ErrorMessage ="Veuillez sélectionner une date valide.")]
         public DateOnly StartDate { get; set; }
         [Required(ErrorMessage ="Le nom de la formation est obligatoire.")]
         public string CourseName { get; set; }
